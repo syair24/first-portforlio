0cd2a035cde0a6e5e2adf973faf41f6d161137aa
syair24 <syair surya samudra24@gmail.com>
2025-08-18 13:26:51 +0700

    first portfolio

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// nav bar
+let navList = document.querySelector(".nav-list");
+let hamburger = document.querySelector(".hamburger");
+let closeBtn = document.querySelector(".close");
+
+function openMenu() {
+  navList.classList.add("open");
+  document.body.classList.add("no-scroll"); // blok scroll
+}
+
+function closeMenu() {
+  navList.classList.remove("open");
+  document.body.classList.remove("no-scroll"); // aktifkan scroll
+}
+
+hamburger.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
+
+// nav bar
